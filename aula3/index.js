c268844e2b104281bf954f48def680bd94ad2051
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2020-02-03 23:33:05 -0300

    Revisão do basico: console.log e comentários

---
+++ aula3/index.js
@@ -0,0 +1,13 @@
+//metodo log dentro do objeto console
+console.log('Luiz Otávio'); // é necessário exibir entre aspas
+console.log("Luiz Otavio");
+console.log(`Luiz 'Otávio'`); //usado para fazer template string
+
+
+console.log(123445, 15.6, 'Luiz Otávio'); //valor literal, pois é colocado literalmente
+
+//String = texto
+//Number = Número ( no javascript não tem diferenciação entre float e int)
+
+//ponto e virgula no javascript é opcional
+
---
+++ aula4/comentarios.js
@@ -0,0 +1,34 @@
+// Uma anotação
+//Comentário é ignorado pelo motor do javascript
+console.log('Isso não é ignorado');
+// console.log('Isso sim');
+
+
+// engine = motor
+//a engine do node e google chrome são a mesma
+
+console.log('Isso não é um comentário');
+/*
+isso é 
+um 
+comentário 
+longo 
+
+também conhecido como 
+comentário de bloco
+
+*/
+
+
+console.log('Em documentação:');
+
+/**
+ * Um titulo
+ *
+ * explicando algo
+ * 
+ //esse comentário
+ //já estava comentado
+ //e continua comentado
+ * 
+*/
