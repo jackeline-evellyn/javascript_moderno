cfefe917b405dcfe77702759721c9a88b9526d19
Jackeline Evellyn <jackelineeteixeira@gmail.com>
2020-02-04 00:02:33 -0300

    Navegador vs Node (HTML + JavaScript)

---
+++ aula5/index.js
@@ -0,0 +1,7 @@
+console.log("teste");
+console.log('Olá mundo denovo');    
+alert("Isso não é executado no terminal");
+//mas no navegador sim
+
+//os ambientes do navegador e node 
+//se comportam de formas diferentes
